6de3115bb33ba998630378f8ac38599385649d7a
vignesh <me@vikki.in>
2020-05-19 17:18:11 +0530

    adding shebang

--- vikki_scripts/sync_to_cdn.sh
+++ vikki_scripts/sync_to_cdn.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #sync to CDN - https://github.com/vignesh88/cdn
 cp -rvf /home/vikki/Dropbox/development/django/tools/app_base64/static_files/base64/* /home/vikki/github/cdn/django/tools/base64/.
 cp -rvf /home/vikki/Dropbox/development/django/tools/app_general/static_files/general/* /home/vikki/github/cdn/django/tools/general/.
